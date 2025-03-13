7c23c80843233c23f1998d43a614e96a809a0981
Judalm Park (dami) <wnekfa1004@naver.com>
2025-03-13 23:18:13 +0900

    [init]

---
+++ src/test/java/io/dami/blogcode/BlogCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dami.blogcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCodeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
